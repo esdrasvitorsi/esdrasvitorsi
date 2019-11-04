800898e1099bd733d5a22f879541f00159782ca2
Esdras Vitor Silva Pinto <vitoresdras@hotmail.com>
2019-11-03 22:18:53 -0300

    Debug $_FILE projero Time Lapse

---
+++ g2n/time-lapse/index.php
@@ -0,0 +1,11 @@
+<?php
+// Nas versões do PHP anteriores a 4.1.0, $HTTP_POST_FILES deve ser utilizado ao invés
+// de $_FILES.
+
+
+echo 'Aqui está mais informações de debug:';
+print_r($_FILES);
+
+print "</pre>";
+
+?>
